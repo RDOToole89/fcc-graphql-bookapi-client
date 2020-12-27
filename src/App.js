609abb6b07834476e33d127155818531cb2f35bc
RDOToole89 <roibinotoole@gmail.com>
2020-12-27 17:53:42 +0100

    Add controlled form to addBook component

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div id='main'>
-        <h1>Ninja's Reading List</h1>
+        <h1>Roibin's Reading List</h1>
         <Booklist />
         <AddBook />
       </div>
--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,32 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { getAuthorsQuery } from '../graphql/queries';
 
 function AddBook(props) {
+  const [newBook, setNewBook] = useState({
+    name: '',
+    genre: '',
+    authorId: '',
+  });
+
   // useQuery Hook returns an object from Apollo Client that contains loading , error
   // , and data properties you can use to render your UI.
   const { loading, error, data } = useQuery(getAuthorsQuery);
   const { authors } = data || [];
 
-  if (error) {
-    return <p>Error! ${error.message}</p>;
-  }
+  if (error) return <p>Error! ${error.message}</p>;
 
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    console.log('New Book Added', newBook);
+  };
   return (
-    <form id='add-book'>
+    <form onSubmit={handleSubmitForm} id='add-book'>
       <div className='field'>
         <label>Book name:</label>
-        <input type='text' />
+        <input
+          type='text'
+          value={newBook.name}
+          onChange={(e) => setNewBook({ ...newBook, name: e.target.value })}
+        />
       </div>
 
       <div className='field'>
         <label>Genre:</label>
-        <input type='text' />
+        <input
+          type='text'
+          value={newBook.genre}
+          onChange={(e) => setNewBook({ ...newBook, genre: e.target.value })}
+        />
       </div>
 
       <div className='field'>
         <label>Author:</label>
-        <select>
+        <select
+          value={newBook.authorId}
+          onChange={(e) => setNewBook({ ...newBook, authorId: e.target.value })}
+        >
           <option>Select Author</option>
           {loading ? (
             <option disabled>Loading authors...</option>
@@ -40,7 +59,7 @@ function AddBook(props) {
         </select>
       </div>
 
-      <button>+</button>
+      <button type='submit'>+</button>
     </form>
   );
 }
