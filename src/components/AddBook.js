5f1727f92641c6eb6b96045dc655a418c7b050c0
RDOToole89 <roibinotoole@gmail.com>
2020-12-27 19:17:05 +0100

    Git revert tryout

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,12 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import { addBookMutation, getAuthorsQuery } from '../graphql/queries';
-import { ADD_BOOK } from '../graphql/mutations';
-import { useMutation } from '@apollo/client';
+import { getAuthorsQuery } from '../graphql/queries';
 
 function AddBook(props) {
-  const [addBook, { bookData }] = useMutation(ADD_BOOK);
-
   const [newBook, setNewBook] = useState({
     name: '',
     genre: '',
@@ -18,59 +14,44 @@ function AddBook(props) {
   const { loading, error, data } = useQuery(getAuthorsQuery);
   const { authors } = data || [];
 
-  if (error) return <p>Error! ${error.message}</p>;
+  if (error) {
+    return <p>Error! ${error.message}</p>;
+  }
 
   const handleSubmitForm = (e) => {
     e.preventDefault();
-    addBook({
-      variables: { name: newBook.name, genre: newBook.genre, authorId: newBook.authorId },
-    });
-
     console.log('New Book Added', newBook);
   };
 
-  return (
-    <form onSubmit={handleSubmitForm} id='add-book'>
-      <div className='field'>
-        <label>Book name:</label>
-        <input
-          type='text'
-          value={newBook.name}
-          onChange={(e) => setNewBook({ ...newBook, name: e.target.value })}
-        />
-      </div>
+  <form id='add-book'>
+    <div className='field'>
+      <label>Book name:</label>
+      <input type='text' />
+    </div>
 
-      <div className='field'>
-        <label>Genre:</label>
-        <input
-          type='text'
-          value={newBook.genre}
-          onChange={(e) => setNewBook({ ...newBook, genre: e.target.value })}
-        />
-      </div>
+    <div className='field'>
+      <label>Genre:</label>
+      <input type='text' />
+    </div>
 
-      <div className='field'>
-        <label>Author:</label>
-        <select
-          value={newBook.authorId}
-          onChange={(e) => setNewBook({ ...newBook, authorId: e.target.value })}
-        >
-          <option>Select Author</option>
-          {loading ? (
-            <option disabled>Loading authors...</option>
-          ) : (
-            authors.map((x) => (
-              <option key={x.id} value={x.id}>
-                {x.name}
-              </option>
-            ))
-          )}
-        </select>
-      </div>
+    <div className='field'>
+      <label>Author:</label>
+      <select>
+        <option>Select Author</option>
+        {loading ? (
+          <option disabled>Loading authors...</option>
+        ) : (
+          authors.map((x) => (
+            <option key={x.id} value={x.id}>
+              {x.name}
+            </option>
+          ))
+        )}
+      </select>
+    </div>
 
-      <button type='submit'>+</button>
-    </form>
-  );
+    <button>+</button>
+  </form>;
 }
 
 export default AddBook;
